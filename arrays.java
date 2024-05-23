3709580d8f6d5a6cd1e285d6bab236a016c6db90
sharath <alsharath66@gmail.com>
2024-05-23 13:48:07 +0530

    java practice

---
+++ arrays.java
@@ -0,0 +1,18 @@
+import java.util.java;
+{
+
+    public class day4{
+        public ststic void main(){
+
+            int arrr[4]={2,3,4,5,7};  //array initilization 
+            System.out.println("the value is :"arr[];)
+            
+
+
+
+
+
+
+        }
+    }
+}
