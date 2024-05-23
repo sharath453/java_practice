fa8935648a5a0d499d46d06eaaa652871cb2a427
sharath <alsharath66@gmail.com>
2024-05-23 13:52:18 +0530

    java practice

--- arrays.java
+++ arrays.java
@@ -1,18 +1,45 @@
-import java.util.java;
-{
+package day4;
+import java.util.Iterator;
+import java.util.Scanner;
+public class arrays {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		
+		
+		int arr[]; // declaration
+		arr = new int[4]; // initilization with new key word
+		System.out.println(arr); // it will print the address of the array
+		System.out.println(arr.length); // it will print length of an array
 
-    public class day4{
-        public ststic void main(){
+		for (int b : arr) { // for each loop
+			System.out.println(b);
+		}
 
-            int arrr[4]={2,3,4,5,7};  //array initilization 
-            System.out.println("the value is :"arr[];)
-            
+		int a[] = { 2, 3, 4, 5, 6, 8 }; // array initilization in other ways
+		for (int i = 0; i < a.length; i++) {
+			System.out.println(a[i]);
+		}
+		
+		
+		
+		int array[];
+		System.out.println("enter the size of array :");
+		int size=sc.nextInt();
+		array=new int[size];
+		
+		
+		System.out.println("enter the value of array :");
+		for(int i=0;i<size;i++)
+		{
+			array[i]=sc.nextInt();	
+		}
+		
+		
+		System.out.println("the array values are :");
+		for(int b:array)
+		{
+			System.out.println(b);
+		}
 
-
-
-
-
-
-        }
-    }
+	}
 }
